41cdb95d37492aa6322f573a7271e8e73fb38abc
Kostiantyn <kosssm155@gmail.com>
2020-08-10 23:40:41 +0300

    clean code

--- src/lesson-2-3/app.js
+++ src/lesson-2-3/app.js
@@ -35,67 +35,7 @@ export const Accordion = ({ source }) => {
 	return (
 		<section className={Styles.accordion}>
 			<h1>Accordion</h1>
-
 			{divJSX}
-
-			{/* <div className={`${Styles["accordion-item"]} ${Styles["selected"]}`}>
-				<div className={Styles["accordion-question"]}>
-					<span>{source[0].question}</span>
-				</div>
-				<p className={Styles["accordion-answer"]}>{source[0].answer}</p>
-			</div>
-
-			<div className={Styles["accordion-item"]}>
-				<div className={Styles["accordion-question"]}>
-					<span>{source[1].question}</span>
-				</div>
-				<p className={Styles["accordion-answer"]}>{source[1].answer}</p>
-			</div>
-
-			<div className={Styles["accordion-item"]}>
-				<div className={Styles["accordion-question"]}>
-					<span>{source[2].question}</span>
-				</div>
-				<p className={Styles["accordion-answer"]}>{source[2].answer}</p>
-			</div> */}
 		</section>
 	);
-
-	// const newsDataArr = [
-	// 	{
-	// 		title: "American writer of",
-	// 		description: "Lorem ipsum",
-	// 		published: "2019-10-15T15:32:45.167Z",
-	// 		likes: 315,
-	// 		comments: 10,
-	// 		image:
-	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	// 		tags: ["literature", "books"],
-	// 	},
-	// 	{
-	// 		title: "Another article",
-	// 		description: "Lorem ipsum",
-	// 		published: "2019-10-15T15:32:45.167Z",
-	// 		likes: 201,
-	// 		comments: 12,
-	// 		image:
-	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	// 		tags: ["music", "sport"],
-	// 	},
-	// 	{
-	// 		title: "Popular article",
-	// 		description: "Lorem ipsum",
-	// 		published: "2019-10-15T15:32:45.167Z",
-	// 		likes: 456,
-	// 		comments: 42,
-	// 		image:
-	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	// 		tags: ["article", "art"],
-	// 	},
-	// ];
-	// return (
-	// 	<>
-	// 		<News source={newsDataArr} />
-	// 	</>
-	// );
 };
