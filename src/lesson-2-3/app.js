435c137deae464817ea21c369acc3e13a2be22e9
Kostiantyn <kosssm155@gmail.com>
2020-08-10 22:37:30 +0300

    lesson-2-3 have to do

---
+++ src/lesson-2-3/app.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+// import { News } from "./components/News/News.js";
+import Styles from "./app.module.scss";
+
+export const App = ({ source }) => {
+	// console.log(Styles);
+	// console.log(source);
+	const toggleHandler = () => {
+		source[0].isPressed = true;
+	};
+
+	let divJSX = source.map((obj, index) => {
+		return (
+			<div
+				key={index}
+				className={
+					obj.isPressed
+						? `${Styles["accordion-item"]} ${Styles["selected"]}`
+						: `${Styles["accordion-item"]}`
+				}
+				onClick={toggleHandler}
+			>
+				<div className={Styles["accordion-question"]}>
+					<span>{obj.question}</span>
+				</div>
+				<p className={Styles["accordion-answer"]}>{obj.answer}</p>
+			</div>
+		);
+	});
+
+	return (
+		<section className={Styles.accordion}>
+			<h1>Accordion</h1>
+
+			{divJSX}
+
+			{/* <div className={`${Styles["accordion-item"]} ${Styles["selected"]}`}>
+				<div className={Styles["accordion-question"]}>
+					<span>{source[0].question}</span>
+				</div>
+				<p className={Styles["accordion-answer"]}>{source[0].answer}</p>
+			</div>
+
+			<div className={Styles["accordion-item"]}>
+				<div className={Styles["accordion-question"]}>
+					<span>{source[1].question}</span>
+				</div>
+				<p className={Styles["accordion-answer"]}>{source[1].answer}</p>
+			</div>
+
+			<div className={Styles["accordion-item"]}>
+				<div className={Styles["accordion-question"]}>
+					<span>{source[2].question}</span>
+				</div>
+				<p className={Styles["accordion-answer"]}>{source[2].answer}</p>
+			</div> */}
+		</section>
+	);
+
+	// const newsDataArr = [
+	// 	{
+	// 		title: "American writer of",
+	// 		description: "Lorem ipsum",
+	// 		published: "2019-10-15T15:32:45.167Z",
+	// 		likes: 315,
+	// 		comments: 10,
+	// 		image:
+	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	// 		tags: ["literature", "books"],
+	// 	},
+	// 	{
+	// 		title: "Another article",
+	// 		description: "Lorem ipsum",
+	// 		published: "2019-10-15T15:32:45.167Z",
+	// 		likes: 201,
+	// 		comments: 12,
+	// 		image:
+	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	// 		tags: ["music", "sport"],
+	// 	},
+	// 	{
+	// 		title: "Popular article",
+	// 		description: "Lorem ipsum",
+	// 		published: "2019-10-15T15:32:45.167Z",
+	// 		likes: 456,
+	// 		comments: 42,
+	// 		image:
+	// 			"https://images.pexels.com/photos/2967810/pexels-photo-2967810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	// 		tags: ["article", "art"],
+	// 	},
+	// ];
+	// return (
+	// 	<>
+	// 		<News source={newsDataArr} />
+	// 	</>
+	// );
+};
---
+++ src/lesson-2-3/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./app";
+import * as serviceWorker from "../serviceWorker";
+ReactDOM.render(
+	<App
+		source={[
+			{
+				question: "What is your name?",
+				answer: "My name is Andrii",
+				isPressed: false,
+			},
+			{
+				question: "What is your name?",
+				answer: "My name is Andrew",
+				isPressed: false,
+			},
+			{
+				question: "What is your name?",
+				answer: "My name is Jane",
+				isPressed: false,
+			},
+		]}
+	/>,
+	document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
